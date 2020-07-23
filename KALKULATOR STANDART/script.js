350aa6940fdfc7f1ab166e0e7de83fceee50abd3
Yogie Setiawan <53132950+setia1yogi@users.noreply.github.com>
2020-07-23 23:28:42 +0700

    Add files via upload

---
+++ KALKULATOR STANDART/script.js
@@ -0,0 +1,18 @@
+let tombol = document.querySelector(".container-tombol");
+let layar = document.querySelector('#layar');
+
+tombol.addEventListener("click", function(e){
+    var tombolClick = e.target;
+    var nilaiTombol = tombolClick.innerText;
+
+    if (nilaiTombol == "C") {
+        layar.value = "";
+    }else if (nilaiTombol == "<") {
+        layar.value = layar.value.slice(0, -1);
+    } else if (nilaiTombol == "=") {
+        layar.value = eval(layar.value);
+    }else{
+        layar.value = layar.value + nilaiTombol;
+    }
+
+});
